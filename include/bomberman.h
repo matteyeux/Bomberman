40c27000c25fe0f29db195f4db2c53419a001ba2
mathieu <mathieu.hautebas@gmail.com>
2019-01-17 12:05:46 +0100

    one player support

--- include/bomberman.h
+++ include/bomberman.h
@@ -1,1 +1,5 @@
+#include <include/interface.h>
+#include <include/player.h>
+
 void bomberman_print(void);
+void destroy_game(interface_t *interface, player_t *player);
--- src/main.c
+++ src/main.c
@@ -2,20 +2,31 @@
 #include <stdlib.h>
 
 #include <include/interface.h>
+#include <include/player.h>
+#include <include/bomberman.h>
 
 int main(void)
 {
 	interface_t *interface = NULL;
+	player_t *player = NULL;
 
 	interface = init_interface();
 
-	if (interface != NULL) {
-		fprintf(stdout, "Successfully initialized interface !\n");
-		SDL_Delay(3000);
-	} else {
-		printf("error\n");
+	if (interface == NULL) {
 		return (EXIT_FAILURE);
 	}
 
-	destroy_interface(interface);
+	fprintf(stdout, "Successfully initialized interface !\n");
+	SDL_Delay(3000);
+
+	player = init_player(interface);
+
+	if (player == NULL) {
+		return (EXIT_FAILURE);
+	}
+
+	fprintf(stdout, "Successfully initialized player !\n");
+	SDL_Delay(3000);
+
+	destroy_game(interface, player);
 }
