2f2df147866b83f6f0dd83a1229686fa60074d15
yoannhu <yoann.hum@gmail.com>
2019-02-02 09:22:25 +0100

    Clean code

--- src/main.c
+++ src/main.c
@@ -14,46 +14,27 @@ int main(void)
 	int status = 0;
 
 	interface = init_interface();
-
 	if (interface == NULL) {
 		return (EXIT_FAILURE);
 	}
-
 	fprintf(stdout, "Successfully initialized interface !\n");
 
 	player = init_player(interface);
-
 	if (player == NULL) {
 		return (EXIT_FAILURE);
 	}
+	fprintf(stdout, "Successfully initialized player !\n");
 
 	bomb = init_bomb(interface);
-	//bomb = newBomb(interface);
-
 	if (bomb == NULL) {
-		//return (EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-
-	fprintf(stdout, "Successfully initialized player !\n");
+	fprintf(stdout, "Successfully initialized bomb !\n");
 
 	while (status != -1) {
 		draw_game(interface, player, bomb);
 
 		status = game_event(player, interface, bomb);
-
-
-		//printf("%d %s", bomb->bombPositionRect.x, "\n");
-
-		/*
-		if (bomb) {
-			printf("%d %s %d %s", 0, " - ", 0, "\n");
-			SDL_Delay(1000);
-			destroy_game(interface, player);
-			SDL_Delay(1000);
-		}else{
-			printf("%s", "null\n");
-		}
-		*/
 
 		SDL_Delay(20);
 	}
