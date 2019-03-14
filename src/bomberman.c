dea074fa182e9a3680bfd526449af4a6a0331a5c
alexandre <majda_a@etna-alternance.net>
2019-03-14 20:29:07 +0100

    correction mauvaise assignation map

--- src/bomberman.c
+++ src/bomberman.c
@@ -21,7 +21,7 @@ void draw_game(game_t *game)
 	SDL_SetRenderDrawColor(interface->Renderer, 16, 120, 48, 255);
 	SDL_RenderClear(interface->Renderer);
 
-	map_t *map = interface->map;
+	map_t *map = game->map;
          
     for (int i = 0; i < map->nbTileY; i++) {
         for (int j = 0; j < map->nbTileX; j++) {
