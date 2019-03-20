1b3705e2b606a76780c875d5e1fb6efa13213d88
mathieu <mathieu.hautebas@gmail.com>
2019-03-20 17:20:29 +0100

    add script to push to svn

---
+++ scripts/commit2svn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# SVN sucks
+
+rm -rf ~/svn
+mkdir -p ~/svn
+svn co --username hauteb_m "https://rendu-svn.etna-alternance.net/v2/2021_Bachelor%20-%20Octobre_TIC-DVC3_9_0-5034/Bomberman/hauteb_m/" ~/svn/bomberman
+
+cp -r fonts images include Makefile map.txt README.md src ~/svn/bomberman/
+cd ~/svn/bomberman && svn add --force * && svn commit -m "update files"
