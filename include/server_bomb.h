ab42358fd260ad312e84668823c52dc40ee5bd45
mathieu <mathieu.hautebas@gmail.com>
2019-04-02 10:53:00 +0200

    fix a bug where bomb data is reset when another player drops a bomb

--- include/server_bomb.h
+++ include/server_bomb.h
@@ -3,17 +3,6 @@
 
 #include <include/server.h>
 
-typedef struct bomb_server_s bomb_server_t;
-
-struct bomb_server_s
-{
-    int x;
-    int y;
-    int player;
-    long time;
-    bomb_server_t *next;
-};
-
 bomb_server_t *init_bomb_server();
 
 void create_new_bomb(t_server_game *server_game, bomb_server_t *server_bomb, int player);
