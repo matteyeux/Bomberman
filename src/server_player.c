4b24c4f1ef284434d99d4ece38b2ecacab296c0b
YoannHumeau <yoann.hum@gmail.com>
2019-04-05 01:29:53 +0200

    Players can't go on other player places

--- src/server_player.c
+++ src/server_player.c
@@ -68,22 +68,22 @@ void player_move(t_server_game *server_game, int player, char command)
     switch (command)
     {
         case 'U' :
-            if (place_is_free(server_game, the_player->x_pos, the_player->y_pos-1)) {
+            if (place_is_free(server_game, the_player->x_pos, the_player->y_pos-1) && place_is_free_of_player(server_game, the_player->x_pos, the_player->y_pos-1)) {
                 the_player->y_pos--;
             }
             break;
         case 'D' :
-            if (place_is_free(server_game, the_player->x_pos, the_player->y_pos+1)) {
+            if (place_is_free(server_game, the_player->x_pos, the_player->y_pos+1) && place_is_free_of_player(server_game, the_player->x_pos, the_player->y_pos+1)) {
                 the_player->y_pos++;
             }
             break;
         case 'L' :
-            if (place_is_free(server_game, the_player->x_pos-1, the_player->y_pos)) {
+            if (place_is_free(server_game, the_player->x_pos-1, the_player->y_pos) && place_is_free_of_player(server_game, the_player->x_pos-1, the_player->y_pos)) {
                 the_player->x_pos--;
             }
             break;
         case 'R' :
-            if (place_is_free(server_game, the_player->x_pos+1, the_player->y_pos)) {
+            if (place_is_free(server_game, the_player->x_pos+1, the_player->y_pos) && place_is_free_of_player(server_game, the_player->x_pos+1, the_player->y_pos)) {
                 the_player->x_pos++;
             }
             break;
@@ -93,6 +93,31 @@ void player_move(t_server_game *server_game, int player, char command)
 bool place_is_free(t_server_game *server_game, int x, int y)
 {
     if (server_game->schema[y][x] == '0' || server_game->schema[y][x] == '1'|| server_game->schema[y][x] == '2')
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool place_is_free_of_player(t_server_game *server_game, int x, int y)
+{
+    if (server_game->player1.x_pos == x && server_game->player1.y_pos == y && server_game->player1.live == 1)
+    {
+        return false;
+    }
+
+    if (server_game->player2.x_pos == x && server_game->player2.y_pos == y && server_game->player2.live == 1)
+    {
+        return false;
+    }
+
+    if (server_game->player3.x_pos == x && server_game->player3.y_pos == y && server_game->player3.live == 1)
+    {
+        return false;
+    }
+
+    if (server_game->player4.x_pos == x && server_game->player4.y_pos == y && server_game->player4.live == 1)
     {
         return false;
     }
