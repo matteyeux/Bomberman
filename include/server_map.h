b2d2138479f40cb0399aaafdb860d57f42a17b95
YoannHumeau <yoann.hum@gmail.com>
2019-04-05 08:25:48 +0200

    Split implement map in different method

--- include/server_map.h
+++ include/server_map.h
@@ -5,6 +5,10 @@
 
 void implement_map(t_server_game *server_game, bomb_server_t *server_bomb, explosion_server_t *server_explosion);
 
+void implement_player_on_map(t_server_game *server_game);
+void implement_bomb_on_map(t_server_game *server_game, bomb_server_t *server_bomb);
+void implement_explosion_on_map(t_server_game *server_game, explosion_server_t *server_explosion);
+
 void put_player_on_map(t_server_game *server_game);
 
 #endif //BOMBERMAN_SERVER_MAP_H
