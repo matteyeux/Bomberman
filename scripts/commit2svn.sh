c6a1473c20672ad2364181d0bbe7df738621f205
mathieu <mathieu.hautebas@gmail.com>
2019-04-05 09:08:26 +0200

    add file to copy and chmod +x script

--- scripts/commit2svn.sh
+++ scripts/commit2svn.sh
@@ -5,5 +5,5 @@ rm -rf ~/svn
 mkdir -p ~/svn
 svn co --username hauteb_m "https://rendu-svn.etna-alternance.net/v2/2021_Bachelor%20-%20Octobre_TIC-DVC3_9_0-5034/Bomberman/hauteb_m/" ~/svn/bomberman
 
-cp -r fonts images include Makefile map.txt README.md src ~/svn/bomberman/
+cp -r doxyfile fonts images include Makefile map.txt README.md src ~/svn/bomberman/
 cd ~/svn/bomberman && svn add --force * && svn commit -m "update files"
